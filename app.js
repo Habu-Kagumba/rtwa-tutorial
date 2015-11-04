8db1d44a9396df815dfc38f41746167865f966a1
Herbert Kagumba <herbert@regulusweb.com>
2015-11-04 17:25:28 +0300

    all abt React state

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-let channels = [
-    {name: 'Hardware Support'},
-    {name: 'Software Support'}
-];
-
 class Channel extends React.Component{
     onClick() {
         console.log(this.props.name);
@@ -29,21 +24,54 @@ class ChannelList extends React.Component{
 }
 
 class ChannelForm extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    onSubmit(e) {
+        let { channelName } = this.state;
+        this.setState({
+            channelName: ''
+        });
+        this.props.addChannel(channelName);
+        e.preventDefault();
+    }
+    onChange(e) {
+        this.setState({
+            channelName: e.target.value
+        });
+    }
     render() {
         return (
-            <form>
-                <input type="text"/>
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <input type="text" onChange={this.onChange.bind(this)} value={this.state.channelName}/>
             </form>
         )
     }
 }
 
 class ChannelSection extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            channels: [
+                {name: 'Hardware Support'},
+                {name: 'Software Support'}
+            ]
+        }
+    }
+    addChannel(name) {
+        let {channels} = this.state;
+        channels.push({name: name});
+        this.setState({
+            channels: channels
+        });
+    }
     render() {
         return (
             <div>
-                <ChannelList channels={channels} />
-                <ChannelForm />
+                <ChannelList channels={this.state.channels} />
+                <ChannelForm addChannel={this.addChannel.bind(this)}/>
             </div>
         )
     }
